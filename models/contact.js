f9f9213ed58f57e2bec28e14ef107fef09fda646
ParagVadher <vadherparag@gmail.com>
2022-08-08 21:19:36 +0530

    init

---
+++ models/contact.js
@@ -0,0 +1,17 @@
+// require mongoose
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    }
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+module.exports=Contact;
